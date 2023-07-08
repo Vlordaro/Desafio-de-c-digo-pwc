822fe3fac48bfd416315fe4319fd89c8acf1f6c8
Vinicius Alves Lordaro <106718023+Vlordaro@users.noreply.github.com>
2023-07-08 16:52:23 -0300

    Create Anagrama de Palindromo.java
    Neste exemplo, a função "AnagramOfPalindrome" recebe uma palavra como entrada e conta a frequência de cada caractere usando um "HashMap". Em seguida, verifica se no máximo um caractere tem frequência ímpar. Se esse critério for atendido, a função retorna "true", indicando que a palavra é um anagrama de um palíndromo. Caso contrário, retorna false.
    
    No método "main", é fornecida uma palavra de exemplo, "radar", e é verificado se ela é um anagrama de um palíndromo. O resultado é impresso no console. Você pode substituir a palavra de exemplo por qualquer outra palavra que desejar testar.

---
+++ Anagrama de Palindromo.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+public class anagrama {
+    
+ public static boolean AnagramOfPalindrome(String word) {
+        // Conta a frequência de cada caractere na palavra
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        
+        // Verifica se no máximo um caractere tem frequência ímpar
+        int oddCount = 0;
+        for (int count : charCount.values()) {
+            if (count % 2 != 0) {
+                oddCount++;
+            }
+            if (oddCount > 1) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        String word = "racecar";
+        if (AnagramOfPalindrome(word)) {
+            System.out.println(" true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
